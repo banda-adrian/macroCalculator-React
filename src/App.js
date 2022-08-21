5cbfc9df43dd38fbfef19dd30e7879655ba47853
Adrian Banda <5205bda@gmail.com>
2022-08-20 21:16:48 -0500

    form in MacroCal.jsx

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import MacroCal from "./components/MacroCal";
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-    <MacroCal />
+      <MacroCal />
     </div>
   );
 }
--- src/components/MacroCal.jsx
+++ src/components/MacroCal.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
 
 const MacroCal = () => {
-  return (
-    <div>MacroCal</div>
-  )
+    return (
+        <div>
+
+            <form>
+                <h1>Macro Calculator</h1>
+
+                <label>Age</label>
+                <input type='number' name='age' placeholder='Enter your Age in years' />
+
+                <div>
+                    <label>Sex</label>
+                    <input type='radio' /> <label>Male</label>
+                    <input type='radio' name='sex' /> <label>Female</label>
+                </div>
+                <div>
+                    <label>Weight (lbs)</label>
+                    <input type="number" placeholder='Enter your weight in pounds' />
+                </div>
+
+                <div>
+                    <label>Height (inches)</label>
+                    <input type="number" placeholder='Enter your height in inches' />
+                </div>
+
+                <div>
+                    <label>Activity Level</label>
+                    <select>
+                        <option>Select an Activity Level</option>
+                        <option>Little to no exercise</option>
+                        <option>Light exercise (1−3 days per week)</option>
+                        <option>Moderate exercise (3−5 days per week)</option>
+                        <option>Heavy exercise (6−7 days per week)</option>
+                        <option>Very heavy exercise (twice per day, extra heavy workouts)</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label>Choose Goal:</label>
+                    <select>
+                        <option>Select a Goal</option>
+                        <option>Cut</option>
+                        <option>Stay the Same Weight</option>
+                        <option>Bulk</option>
+                    </select>
+                </div>
+
+                <div>
+                    <button type="reset">Reset</button>
+                    <button type="submit">Calculate</button>
+                </div>
+
+            </form>
+            
+        </div>
+    )
 }
 
 export default MacroCal
