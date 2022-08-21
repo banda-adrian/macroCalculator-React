fda6847058b3ff6c96e1ea19d41e1d6ada97898a
Adrian Banda <5205bda@gmail.com>
2022-08-20 20:58:54 -0500

    added tailwindcss/ created MacroCal.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import MacroCal from "./components/MacroCal";
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+    <MacroCal />
     </div>
   );
 }
---
+++ src/components/MacroCal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MacroCal = () => {
+  return (
+    <div>MacroCal</div>
+  )
+}
+
+export default MacroCal
